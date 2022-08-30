14db39b701ad26733adb9deda950a1193c2ba4b1
Tsvetelina Karamihova <ts.karamihova@gmail.com>
2022-08-30 20:01:53 +0300

    Exercise Arrays Advanced

---
+++ ArraysAdvanced/firstAndLastKNums.js
@@ -0,0 +1,18 @@
+function solve(array){
+
+    let k = array[0];
+    let newArray = array.slice(1);
+
+    let first = newArray.slice(0, k);
+    let index = newArray.length - k;
+    let last = newArray.splice(index, k);
+
+    console.log(first.join(' '));
+    console.log(last.join(' '));
+}
+
+
+solve([3,
+    6, 7, 8, 9]
+   );
+
---
+++ ArraysAdvanced/sumFirstAndLast.js
@@ -0,0 +1,10 @@
+function solve(array){
+
+    let a = Number(array[0]);
+    let b = Number(array[array.length - 1]);
+
+    let result = a + b;
+    console.log(result);
+}
+
+solve(['20', '30', '40']);
