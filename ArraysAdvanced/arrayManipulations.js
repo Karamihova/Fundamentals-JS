d32dcda4164bca01b6e9265e219cd1abbab0ccfe
Tsvetelina Karamihova <ts.karamihova@gmail.com>
2022-09-05 12:26:53 +0300

    Arrays Exercise

--- ArraysAdvanced/arrayManipulations.js
+++ ArraysAdvanced/arrayManipulations.js
@@ -1,3 +1,43 @@
-function solve(){
+function solve(input){
     
+    let array = input
+                .shift()
+                .split(' ')
+                .map(Number);
+
+    
+                
+    //destructuring
+    for (let i = 0; i < input.length; i++) {
+        
+        let [command, firstNum, secondNum] = input[i].split(' ');
+
+        switch (command) {
+            case "Add":
+                array.push(Number(firstNum));
+                break;
+            case "Remove":
+                array = array.filter(el => el !== Number(firstNum));
+                break;
+            case "RemoveAt":
+                array.splice(Number(firstNum), 1);
+                break;
+            case "Insert":
+                array.splice(Number(secondNum), 0, Number(firstNum));
+                break;
+            default:
+                break;
+        }
+
+        
+    }
+
+    console.log(array.join(' '));
 }
+
+solve(['4 19 2 53 6 43',
+'Add 3',
+'Remove 2',
+'RemoveAt 1',
+'Insert 8 3']
+);
---
+++ ArraysAdvanced/demo.js
@@ -0,0 +1,6 @@
+function demo(arr){
+    let first = arr.shift();
+    console.log(arr.join(' '));
+}
+
+demo([1, 2, 3]);
