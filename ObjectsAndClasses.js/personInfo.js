cc3cb20b58a541a63476def5e8a1de83d58ec602
Tsvetelina Karamihova <ts.karamihova@gmail.com>
2022-09-11 10:48:25 +0300

    Study Objects and Classes

---
+++ ObjectsAndClasses.js/personInfo.js
@@ -0,0 +1,12 @@
+function solve(firstName, lastName, age){
+
+    const person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    };
+
+    return person;
+}
+
+solve("Tsvetelina", "Karamihova", 22);
